e9c807547d09f9f3c32f681bed134fa216649539
koreyconway <korey.conway@gmail.com>
2015-02-24 13:09:19 -0500

    Add tmux OS conditional configuration

--- install.sh
+++ install.sh
@@ -1,20 +1,28 @@
 BASEDIR=$(cd $(dirname $0); pwd;)
+OS=$(uname | tr [[:upper:]] [[:lower:]])
 
 link_file() {
 	# $1 is basename of file
-	[ -z $1 ] && echo "No file given" && return
-	[ -e ~/.${1} ] && [ ! -h ~/.${1} ] \
+	local srcname=$1
+	local destname=$2
+	[ -z $srcname ] && echo "No file given" && return
+	[ -z $destname ] && destname=$srcname
+	[ -e ~/.${destname} ] && [ ! -h ~/.${destname} ] \
 		&& echo "~/.${1} file exists, moving to ~/.{1}.bk" \
-		&&  mv ~/.${1} ~/.${1}.bk
-	[ -h ~/.${1} ] && echo "Overwritting link for ~/.${1}"
-	ln -fs ${BASEDIR}/${1} ~/.${1} \
-		|| echo "Failed to link ${1}"
+		&&  mv ~/.${destname} ~/.${destname}.bk
+	[ -h ~/.${destname} ] && echo "Overwritting link for ~/.${destname}"
+	ln -fs ${BASEDIR}/${srcname} ~/.${destname} \
+		|| echo "Failed to link ${destname}"
+	echo "Linked ~/.${destname}"
 }
 
 link_file "zshrc"
 link_file "tmux.conf"
 link_file "screenrc"
 link_file "vimrc"
+
+[ -f "${BASEDIR}/tmux.conf.${OS}" ] && \
+	link_file "tmux.conf.${OS}" "tmux.conf.os"
 
 if [ ! -e ~/.vim/bundle/neobundle.vim ]; then
 	mkdir -p ~/.vim/bundle
